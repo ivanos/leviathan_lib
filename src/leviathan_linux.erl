4596db38fc915f4fe5608f9e6624392be972f219
sigstop <stu.bailey@gmail.com>
2015-07-29 20:36:34 -0700

    set_netns

--- src/leviathan_linux.erl
+++ src/leviathan_linux.erl
@@ -3,5 +3,8 @@
 -compile(export_all).
 
 
-set_netns(Cid)->ok.
+set_netns(Cid)->
+    CmdBundle = 
+	[leviathan_bash:mkdir_p_var_run_netns(),
+	 leviathan_ln_s_proc_ns_net_var_run_netns(leviathan_docker:inspect_pid(Cid))].
     
