bab9c2006187f969a7bac85166ec9c8dec4b07df
sigstop <stu.bailey@gmail.com>
2015-07-29 23:11:42 -0700

    changed lev peer names

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # leviathan_lib
 Erlang code specific to Leviathan: Docker Container Network Orchestrator
+
+Notes:
+
+
+Linux:
+
+ip links of type veth peer have the following name convention
+
+<container id>.<leviathan peer number>i   - endpoint to be stuffed
+inside the container
+
+<container id>.<leviathan peer number>o   - endpoint to remain
+outside the container
+
+For Example:
+
+4c01db0b339c.0i
+4c01db0b339c.0o
+
+Maximum Levianthan generated veth peers per cotainer is currently set
+at hard limit of 10,000 ranging from 0-9999.  This limit stems from
+the Linux network device name length limits and the Leviathan naming
+convention that embeds the local container ID in each ip link peer
+device name.
+
+Generally the "inside" end of a peer link, will be renamed to
+Eth<leviathan peer number> (e.g. Eth0) when it is assigned the
+namespace for the container.
--- src/leviathan_linux.erl
+++ src/leviathan_linux.erl
@@ -11,20 +11,20 @@ remove_netns(Cid)->
     [leviathan_bash:rm_f_var_run_netns(leviathan_docker:inspect_pid(Cid))].
 
 
-new_peer(Cid)->
-    LevNameOut = mk_peer_lev_name_out(Cid),
-    LevNameIn = mk_peer_lev_name_in(Cid),
+new_peer(Cid,PeerNum)->
+    LevNameOut = mk_peer_lev_name_out(Cid,PeerNum),
+    LevNameIn = mk_peer_lev_name_in(Cid,PeerNum),
     [leviathan_ip:link_add_type_veth_peer_name(LevNameOut,LevNameIn)].
 
 
-mk_peer_lev_name_in(Cid)->
-    mk_peer_lev_name_prefix(Cid) ++ ".in".
+mk_peer_lev_name_in(Cid,PeerNum)->
+    mk_peer_lev_name_prefix(Cid,PeerNum) ++ "i".
 
-mk_peer_lev_name_out(Cid)->
-    mk_peer_lev_name_prefix(Cid) ++ ".out".
+mk_peer_lev_name_out(Cid,PeerNum)->
+    mk_peer_lev_name_prefix(Cid,PeerNum) ++ "o".
 
-mk_peer_lev_name_prefix(Cid)->
-    "levPeer." ++ Cid.
+mk_peer_lev_name_prefix(Cid,PeerNum)->
+    Cid ++ "." ++ integer_to_list(PeerNum).
 
 
 eval(CmdBundle)->
