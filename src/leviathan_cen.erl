4dbf7fa72d592fb873a3afb3d2cace9350ca5909
Marc Sugiyama <sugiyama@acm.org>
2015-07-31 15:14:26 -0700

    Revert "integrate with lucet_dby"

--- src/leviathan_cen.erl
+++ src/leviathan_cen.erl
@@ -29,38 +29,9 @@
 -define(CONTSMAP,#{"conts"=>[?CONT1,?CONT2,?CONT3,?CONT4,?CONT5]}).
 
 
--define(CENSLIST, [<<"cen1">>,<<"cen2">>,<<"cen3">>,<<"cen4">>,<<"cen5">>]).
--define(TESTDATA, <<"{\"cenList\":
- [{
-     \"cenID\" : \"cen1\",
-     \"containerIDs\" : [ \"c1\",\"c2\",\"c13\",\"c14\"]
-  },
-  {
-      \"cenID\":\"cen2\",
-      \"containerIDs\":[\"c4\",\"c5\",\"c6\",\"c7\"]
-  },
-  {
-      \"cenID\":\"cen3\",
-      \"containerIDs\":[\"c15\",\"c16\",\"c9\",\"c10\",\"c11\"]
-  },
-  {
-      \"cenID\":\"cen4\",
-      \"containerIDs\":[\"c11\",\"c12\"]
-  },
-  {
-      \"cenID\":\"cen5\",
-      \"containerIDs\":[\"c2\",\"c3\"]
-  }]
-}">>).
-
-test_import() ->
-    import_cen_binary_to_dobby(?TESTDATA).
-
-test_prepare_cens() ->
-    prepare_cens(?CENSLIST).
-
 -spec import_cen_to_dobby(filename:filename_all()) -> ok | {error, Reason} when
       Reason :: term().
+
 import_cen_to_dobby(Filename) ->
     {ok, Binary} = file:read_file(Filename),
     import_cen_binary_to_dobby(Binary).
@@ -79,18 +50,15 @@ import_cen_binary_to_dobby(Binary) ->
 %
 %
 
-prepare_cens(CenIds) ->
-    CensMaps = lists:map(
-                fun(CenId) ->
-                    lucet_dby:get_cen(CenId)
-                end, CenIds),
-    prepare_cens_maps(CensMaps).
+prepare_cens_test()->
+    prepare_cens(?CENSMAP).
 
-prepare_cens_maps(CenMaps) ->
+prepare_cens(CensMap)->
+    {ok,Cens} = maps:find("cens",CensMap),
 
-    %%  2 passes are required since the second pass assumes that
-    %%  all buses have already been created so that every container  
-    %%  must be touched only once in the second pass
+    %%  2 passes are required sine the second pass assumes that
+    %%  all busses have already be created so that every container  
+    %%  must touched only once in the second pass
     %%
     %%  Pass 1: 
     %%     make any necessary Ethernet buses
@@ -99,30 +67,35 @@ prepare_cens_maps(CenMaps) ->
     lists:foreach(fun(CenMap)->
 			  case get_cen_type(CenMap) of
 			      bus ->
-				  {ok,CenId} = maps:find(cenID, CenMap),
+				  {ok,CenId} = maps:find("cenID",CenMap),
 				  CmdBundle = leviathan_linux:new_bus(CenId),
 				  leviathan_linux:eval(CmdBundle);				      
 			      _ -> ok %% don't create a bus
 			  end
-		  end, CenMaps),
+		  end, Cens),
     
     %%  Pass 2: 
     %%     configure containers for each Cen
     %%     and create any pipes for Cens with exactly 2 containers
-    lists:foreach(fun(CenMap)->prepare_cen(CenMap) end, CenMaps).
+    lists:foreach(fun(CenMap)->prepare_cen(CenMap) end, Cens).
+
+
+
 
 get_cen_type(CenMap) ->
-    {ok,ContIds} = maps:find(contIDs,CenMap),
+    {ok,ContIds} = maps:find("contIDs",CenMap),
     case length(ContIds) of
 	0 -> null;			       
 	1 -> null;
 	2 -> pipe;
 	_ -> bus
     end.
+			      
+
 
 prepare_cen(CenMap)->
     %% get container list
-    {ok,ContIds} = maps:find(contIDs,CenMap),
+    {ok,ContIds} = maps:find("contIDs",CenMap),
 
     %% if there are only 2 containers in this Cen,
     %%
@@ -135,33 +108,35 @@ prepare_cen(CenMap)->
 				  prepare_cont(ContId) end,ContIds)
     end.
 
+
 prepare_cont(ContId)->
-    % XXX optimization - maybe get Container maps all at once; could that
-    % cause overwriting issues?
-    ContMap = lucet_dby:get_cont(ContId),
+
+    % fake call needs to be replace with rea
+    % maps navigation
+    ContMap = get_cont(ContId),
     set_netns(ContId),
 
-    {ok,Cens} = maps:find(cens,ContMap),
+    {ok,Cens} = maps:find("cens",ContMap),
     Update = lists:foldl(fun(Cen,Acc)->
 				 {CenUpdateAcc,PeerNum} = Acc,
 				 CenUpdate = prepare_cont(ContId,Cen,PeerNum),
 				 {CenUpdateAcc++[CenUpdate],PeerNum+1} end,
 			 {[],0},Cens),
-    NewContMap = maps:update(cens,Update,ContMap),
+    NewContMap = maps:update("cens",Update,ContMap),
 
-    %%=== CaLL Lucet ===
+    %%=== CALL Lucet ===
     %%lucet:set_cont(NewConMap),
     
     %fake call
     set_cont(NewContMap).
 
 prepare_cont(ContId,Cen,PeerNum)->
-    {ok,CenId} = maps:find(cenID,Cen),
-    {ok,PeerID} = maps:find(peerId,Cen),
+    {ok,CenId} = maps:find("cenID",Cen),
+    {ok,PeerID} = maps:find("peerId",Cen),
     case PeerID of
 	unassigned ->
 	    create_peer(CenId,ContId,PeerNum),
-	    maps:update(peerId,leviathan_linux:mk_lev_eth_name(PeerNum),Cen);
+	    maps:update("peerId",leviathan_linux:mk_lev_eth_name(PeerNum),Cen);
 	_ ->
 	    ?DEBUG("Peer already created! ContId = ~p, Cen = ~p, PeerNum = ~p",[ContId,Cen,PeerNum]),
 	    Cen
@@ -175,9 +150,31 @@ create_peer(CenId,ContId,PeerNum)->
     CmdBundle = leviathan_linux:new_peer(CenId,ContId,PeerNum),
     leviathan_linux:eval(CmdBundle).
 
+    
 
-set_cont(NewContMap)->
-    ?DEBUG("NewContMap = ~p",[NewContMap]).
+
+
+
+
+
+
+
+%% Fake calls to lucet:get_cen(...) and lucet:get_cont(...) for development
+
+get_cont("c1")->
+    ?CONT1;
+get_cont("c2") ->
+    ?CONT2;
+get_cont("c3") ->
+    ?CONT3;
+get_cont("c4")->
+    ?CONT4;
+get_cont("c5") ->
+    ?CONT5.
+
+
+set_cont(NewContMap)->ok.
+    %%io:format("NewContMap = ~p",[NewContMap]).
     
 
 %% Internal functions
