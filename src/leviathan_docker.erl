384381d61c1a1deb1a5406aa8af755bf7363ac69
sigstop <stu.bailey@gmail.com>
2015-08-13 19:23:44 -0700

    inspect_pid debug

--- src/leviathan_docker.erl
+++ src/leviathan_docker.erl
@@ -13,8 +13,10 @@ inspect_pid
     Result = os:cmd(Cmd),
     %%Result = "justtesting",
     Stripped = string:strip(Result,right,$\n),
+    ?DEBUG("leviathan:inspect_pid(~p) Stripped = ~p!",[Cid,Stripped]),
     case string:to_integer(Stripped) of
-	{_,[]}->
+	{Pid,[]}->
+	    ?DEBUG("leviathan:inspect_pid(~p) Pid = ~p!",[Cid,Pid]),
 	    Stripped;
 	_ -> 
 	    ?DEBUG("leviathan:inspect_pid(~p) BAD Container PID (must be an integer) ~p!",[Cid,Cid]),
