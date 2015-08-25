dfcca45f04bbb6f4e7dd56d62a9a0e02186b96f9
marcsugiyama <sugiyama@acm.org>
2015-08-25 15:19:59 -0700

    Set sup and gen_server for docker event reader

---
+++ src/leviathan_lib_app.erl
@@ -0,0 +1,16 @@
+-module(leviathan_lib_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    leviathan_lib_sup:start_link().
+
+stop(_State) ->
+    ok.
