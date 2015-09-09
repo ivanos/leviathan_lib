f03093da4ba61781816bd1deaa6305e1b510a966
marcsugiyama <sugiyama@acm.org>
2015-09-08 19:23:28 -0700

    refactor to expose more of the search functions

---
+++ include/leviathan_lib.hrl
@@ -0,0 +1,40 @@
+% macros for matching Leviathan identifiers in dobby
+
+% pattern match a field of metadata
+-define(MDVALUE(Key, Var), Key := #{value := Var}).
+
+% pattern match the type of the identifier
+-define(MDTYPE(Type), ?MDVALUE(<<"type">>, Type)).
+
+% pattern match for a container and the container id
+-define(MATCH_CONTAINER(ContId), #{?MDTYPE(<<"container">>),
+                                   ?MDVALUE(<<"contID">>, ContId)}).
+
+% mattern match for a bridge, the bridge id, and the ip address
+-define(MATCH_BRIDGE(BridgeId, IPAddress), #{?MDTYPE(<<"bridge">>),
+                                  ?MDVALUE(<<"bridgeID">>, BridgeId),
+				  ?MDVALUE(<<"ipaddr">>, IPAddress)}).
+
+% pattern match for a CEN, the CEN id, and the wire type
+-define(MATCH_CEN(CenId, WireType), #{?MDTYPE(<<"cen">>),
+                                       ?MDVALUE(<<"cenID">>, CenId),
+                                       ?MDVALUE(<<"wire_type">>, WireType)}).
+
+% pattern match for an endpoint and the endpoint id
+-define(MATCH_ENDPOINT(EndId), #{?MDTYPE(<<"endpoint">>),
+                                 ?MDVALUE(<<"endID">>, EndId)}).
+
+% pattern match for an "inside" endpoint, the endpoint id, and the alias
+-define(MATCH_IN_ENDPOINT(EndId, Alias), #{?MDTYPE(<<"endpoint">>),
+                                          ?MDVALUE(<<"side">>, <<"in">>),
+                                          ?MDVALUE(<<"endID">>, EndId),
+                                          ?MDVALUE(<<"alias">>, Alias)}).
+
+% pattern match for an "outside" endpoint and the endpoint id
+-define(MATCH_OUT_ENDPOINT(EndId), #{?MDTYPE(<<"endpoint">>),
+                                     ?MDVALUE(<<"side">>, <<"out">>),
+                                     ?MDVALUE(<<"endID">>, EndId)}).
+
+% pattern match for the ipaddr metadata field
+-define(MATCH_IPADDR(IpAddr), #{?MDTYPE(<<"ipaddr">>),
+                                ?MDVALUE(<<"ipaddr">>, IpAddr)}).
