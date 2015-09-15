0248097f4859e272dc883f018166c25602c7ce55
Magnus Henoch <magnus.henoch@gmail.com>
2015-09-15 18:53:27 +0100

    Retrieve datapath id by waiting for new switch connection
    We'll need to find a proper way to do this, but this should work well
    enough for now.

--- src/leviathan_switch.erl
+++ src/leviathan_switch.erl
@@ -17,15 +17,35 @@ import_json(#{<<"type">> := CTypeBin,
               <<"interfaces">> := InterfacesBin} = Switch) ->
     CType = binary_to_list(CTypeBin),
     Interfaces = lists:map(fun binary_to_list/1, InterfacesBin),
-    {ok, ContainerId} = run_switch(CType, Interfaces),
-    %% XXX: get the datapath id!
-    DatapathId = <<"this-is-not-the-datapath-id-", ContainerId/binary>>,
+    {ok, ContainerId, DatapathId} = run_switch(CType, Interfaces),
     NewSwitch = Switch#{<<"contID">> => ContainerId,
                         <<"datapath_id">> => DatapathId},
     leviathan_dby:import_switch(<<"host1">>, NewSwitch).
 
 run_switch(CType, Interfaces) ->
+    AlreadyConnected = weave_ofsh:all_connected(),
+
     CmdBundle = [leviathan_docker:run(CType, "--net=host --privileged=true", string:join(Interfaces, " "))],
     [ContainerId] = leviathan_linux:eval(CmdBundle, output),
     io:format("switch results:~n~p~n", [ContainerId]),
-    {ok, ContainerId}.
+
+    %% XXX: Here we wait for a new incoming switch connection, and
+    %% assume that it's coming from the newly started container.  This
+    %% is a potential race condition.
+    DatapathId = wait_for_new_connection(AlreadyConnected),
+
+    {ok, ContainerId, DatapathId}.
+
+wait_for_new_connection(AlreadyConnected) ->
+    wait_for_new_connection(AlreadyConnected, 100).
+
+wait_for_new_connection(_, 0) ->
+    error(switch_connection_timeout);
+wait_for_new_connection(AlreadyConnected, N) when N > 0 ->
+    case weave_ofsh:all_connected() -- AlreadyConnected of
+        [] ->
+            timer:sleep(10),
+            wait_for_new_connection(AlreadyConnected, N - 1);
+        [DatapathId] ->
+            list_to_binary(DatapathId)
+    end.
