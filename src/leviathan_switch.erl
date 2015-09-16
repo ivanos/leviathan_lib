0388fbbfce3a0e28ff6c848861be49696649de02
Magnus Henoch <magnus.henoch@gmail.com>
2015-09-16 17:47:03 +0100

    Document the leviathan_switch module

--- src/leviathan_switch.erl
+++ src/leviathan_switch.erl
@@ -1,16 +1,46 @@
+%% @doc
+%% This module starts an OpenFlow switch and publishes the appropriate
+%% information to Dobby.
+%%
+%% Use it like this:
+%%
+%% ```
+%%     leviathan_switch:import_json(
+%%       #{<<"type">> => <<"local/linc">>,
+%%         <<"interfaces">> => [<<"cen1">>, <<"cen2">>]}).
+%% '''
+%%
+%% The Docker image named in the `type' attribute will be started with
+%% the options `--net=host --privileged=true', and the interface names
+%% will be passed as command line arguments.  The Docker image should
+%% have an `ENTRYPOINT' appropriately set to accept those arguments.
 -module(leviathan_switch).
 
 -export(
    [import_binary/1,
-    import_json/1,
-    run_switch/2]).
+    import_json/1]).
 
 -include("leviathan_logger.hrl").
 
+%% @doc Start one or more switches based on a JSON description.
+%%
+%% This function parses `Binary' as JSON and passes it to {@link
+%% import_json/1}.
 import_binary(Binary) ->
     SwitchMap = jiffy:decode(Binary, [return_maps]),
     import_json(SwitchMap).
 
+%% @doc Start one or more switches, and publish info to Dobby.
+%%
+%% The argument can be a single map, or a list of maps.  Each
+%% map should have two keys:
+%%
+%% The `<<"type">>' key should have a binary value, that names a
+%% Docker image.
+%%
+%% The `<<"interfaces">>' key should have a list of binaries,
+%% each naming an interface name that the newly started switch
+%% should manage.
 import_json(Switches) when is_list(Switches) ->
     lists:foreach(fun import_json/1, Switches);
 import_json(#{<<"type">> := CTypeBin,
@@ -22,6 +52,7 @@ import_json(#{<<"type">> := CTypeBin,
                         <<"datapath_id">> => DatapathId},
     leviathan_dby:import_switch(<<"host1">>, NewSwitch).
 
+%% @doc Start a switch, without publishing anything to Dobby.
 run_switch(CType, Interfaces) ->
     AlreadyConnected = weave_ofsh:all_connected(),
 
